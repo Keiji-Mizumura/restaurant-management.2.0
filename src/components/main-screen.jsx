497dbc1004ecf8aaf7361a16cb76f8676b32bac1
Keiji-Mizumura <keijimizumura@icloud.com>
2023-03-04 15:17:46 +0900

    side menu

--- src/components/main-screen.jsx
+++ src/components/main-screen.jsx
@@ -1,5 +1,8 @@
 import classes from './screen-styles/main-screen.module.css';
 
+import WarningButton from './buttons/warning-button';
+import DarkButton from './buttons/dark-button';
+
 const MainScreen = () => {
     return(
         <div className={classes.container}>
@@ -9,7 +12,27 @@ const MainScreen = () => {
             </div>
             <div className={classes.queue}>
                 {/* QUEUE */}
-                
+                <div className={classes.controls}>
+                    <WarningButton>Call Staff</WarningButton>
+                </div>
+                <div className={classes.orders}>
+                    <h2>Order Contents</h2>
+                    <div className={classes.order_item}>
+                    </div>
+                    <div className={classes.order_item}>
+                    </div>
+                    <div className={classes.order_item}>
+                    </div>
+                    <div className={classes.order_item}>
+                    </div>
+                </div>
+                <div className={classes.total}>
+                    <p>Total: 3,500,000</p>
+                </div>
+                <div className={classes.controls}>
+                    <DarkButton>Clear All</DarkButton>
+                    <WarningButton>Confirm Order</WarningButton>
+                </div>
             </div>
         </div>
     )
