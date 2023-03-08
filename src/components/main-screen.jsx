a51a87c1dc631da47534070f98e0cfec33e2b509
Keiji-Mizumura <keijimizumura@icloud.com>
2023-03-08 10:13:55 +0900

    Trying to update the food queuing system

--- src/components/main-screen.jsx
+++ src/components/main-screen.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import classes from "./screen-styles/main-screen.module.css";
 import Queue from "./ui/queue";
@@ -7,12 +7,26 @@ import DishItems from "./ui/dish-items";
 const MainScreen = (props) => {
 
   const [orderedFood, setOrderedFood] = useState([]);
+  const [orderedFoodId, setOrderedFoodId] = useState([]);
 
   const orderHandler = (foodObject) => {
-    const tempArr = orderedFood.slice();
-    tempArr.push(foodObject);
-    setOrderedFood(tempArr);
+    const checkIfExists = orderedFoodId.some(x => x.id === foodObject.id);
+    if(!checkIfExists){ // First time setting the food
+      const tempArr = orderedFoodId.slice();
+      tempArr.push({id: foodObject.id, amount: 1});
+      setOrderedFoodId(tempArr);
+    }
+    else{ // Increment the amount of food
+      
+    }
+    // const tempArr = orderedFood.slice();
+    // tempArr.push(foodObject);
+    // setOrderedFood(tempArr);
   }
+
+  useEffect(() => {
+    console.log(orderedFoodId);
+  },[orderedFood, orderedFoodId]);
 
   const orderDeleteHander = (foodId) => {
     const tempArr = orderedFood.slice().filter(x => x.id !== foodId);
